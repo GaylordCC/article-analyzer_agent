d70e0a4cc4660b5b05e1ccd0d7a328cca62783f1
GaylordCC <gcarricaballero3@hotmail.com>
2025-04-24 22:34:36 -0500

    Add new multi agent router

--- article_agent/main.py
+++ article_agent/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from .routers import article_analyzer_agent
+from .routers import article_analyzer_agent, multi_agent_supervisor
 
 app = FastAPI()
 
 app.include_router(article_analyzer_agent.router)
+app.include_router(multi_agent_supervisor.router)
